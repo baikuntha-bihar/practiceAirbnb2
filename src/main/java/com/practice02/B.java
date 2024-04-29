09556a6cf4973e7a895e3320af16d2e7f575dd59
baikuntha <baikunthabihar18@gmail.com>
2024-04-30 00:03:22 +0530

    add B class and implement double late is not repeated

---
+++ src/main/java/com/practice02/B.java
@@ -0,0 +1,10 @@
+package com.practice02;
+
+public class B {
+    // first double later should be not repeated in the output and print in string
+    public static void main(String[] args) {
+        String s = "asdf;kjakdjf;akdjadf;lkjadfadf";
+        System.out.println(s.substring(0, 1));
+
+    }
+}
